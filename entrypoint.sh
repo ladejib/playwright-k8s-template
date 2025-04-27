b7d5b8c1da4ae54e6f647e65b537fc0d6c996f6e
ladejib <ladejibiobaku@gmail.com>
2025-04-27 10:03:28 -0400

    updated entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,6 +4,9 @@ set -e
 echo "[entrypoint] Installing dependencies..."
 npm install
 
+echo "[entrypoint] Installing Playwright browsers..."
+npx playwright install --with-deps
+
 echo "[entrypoint] Running Playwright tests..."
 npx playwright test --config "$GIT_SYNC_ROOT/playwright.config.js"
 
